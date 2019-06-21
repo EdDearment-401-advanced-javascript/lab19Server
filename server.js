32be869197dcf408cf509ec525b5a790944a20c1
ekearm <ed787.ed@gmail.com>
2019-06-20 19:18:58 -0700

    server is done need mock and test

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('dotenv').config();
+const Q = require('@nmq/q/server');
+
+Q.start();
+
+const db = new Q('database');
+
+db.monitorEvent('create');
+db.monitorEvent('read');
+db.monitorEvent('update');
+db.monitorEvent('delete');
+
+const network = new Q('network');
+network.monitorEvent('file-save');
+network.monitorEvent('file-error');
